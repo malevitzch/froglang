ebc5a25b0de0337082b1e582698150257d4cdce7
malevitzch <malevitzch@gmail.com>
2024-12-24 13:56:04 +0100

    Retabbed a file

--- include/ast/node.hpp
+++ include/ast/node.hpp
@@ -2,31 +2,31 @@
 #include <llvm/IR/Value.h>
 
 namespace ast {
-// Abstract class
-class Node {
-private:
-  std::string name = "Uninitialized Name";
-protected:
-  Node() = default;
-  Node(std::string name);
-public:
-  virtual llvm::Value* codegen() = 0;
-  std::string get_name();
-};
+  // Abstract class
+  class Node {
+  private:
+    std::string name = "Uninitialized Name";
+  protected:
+    Node() = default;
+    Node(std::string name);
+  public:
+    virtual llvm::Value* codegen() = 0;
+    std::string get_name();
+  };
 
-class GlobjectNode : public Node {
-private:
-public:
-  llvm::Value* codegen() override;
-  GlobjectNode();
-};
+  class GlobjectNode : public Node {
+  private:
+  public:
+    llvm::Value* codegen() override;
+    GlobjectNode();
+  };
 
-class ProgramNode : public Node {
-private:
-  std::vector<Node*> globjects;
-public:
-  llvm::Value* codegen() override;
-  void add_obj(GlobjectNode* globject);
-  ProgramNode();
-};
+  class ProgramNode : public Node {
+  private:
+    std::vector<Node*> globjects;
+  public:
+    llvm::Value* codegen() override;
+    void add_obj(GlobjectNode* globject);
+    ProgramNode();
+  };
 }
