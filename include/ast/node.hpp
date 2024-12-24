f96ca8a151c5c0e009c9aa4f3084559e99e82b78
malevitzch <malevitzch@gmail.com>
2024-12-24 15:06:32 +0100

    Added missing get_name functions to base ast nodes

--- include/ast/node.hpp
+++ include/ast/node.hpp
@@ -16,6 +16,7 @@ namespace ast {
   private:
   public:
     llvm::Value* codegen() override;
+    std::string get_name() override;
     GlobjectNode();
   };
 
@@ -24,6 +25,7 @@ namespace ast {
     std::vector<Node*> globjects;
   public:
     llvm::Value* codegen() override;
+    std::string get_name() override;
     void add_obj(GlobjectNode* globject);
     ProgramNode();
   };
--- src/ast/node.cpp
+++ src/ast/node.cpp
@@ -9,12 +9,17 @@ namespace ast {
     //TODO: implement
   }
   GlobjectNode::GlobjectNode() {}
+  std::string GlobjectNode::get_name() {
+    return "Globject Node";
+  }
 
 
   llvm::Value* ProgramNode::codegen() {
     //TODO: implement
   }
-
+  std::string ProgramNode::get_name() {
+    return "Program Node";
+  }
   void ProgramNode::add_obj(GlobjectNode* globject) {
     globjects.push_back(globject);
   }
