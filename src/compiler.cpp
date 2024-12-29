780a59292c9d50fdc0d24876d2672c03ab2978ee
malevitzch <malevitzch@gmail.com>
2024-12-29 13:40:48 +0100

    Set LLVM up

--- src/compiler.cpp
+++ src/compiler.cpp
@@ -1,7 +1,52 @@
 #include <iostream>
 #include "ast/node.hpp"
-
+#include "llvm/ADT/APFloat.h"
+#include "llvm/ADT/STLExtras.h"
+#include "llvm/IR/BasicBlock.h"
+#include "llvm/IR/Constants.h"
+#include "llvm/IR/DerivedTypes.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/Instructions.h"
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/LegacyPassManager.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/Type.h"
+#include "llvm/IR/Verifier.h"
+#include "llvm/MC/TargetRegistry.h"
+#include "llvm/Support/FileSystem.h"
+#include "llvm/Support/TargetSelect.h"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/Target/TargetMachine.h"
+#include "llvm/Target/TargetOptions.h"
+#include "llvm/TargetParser/Host.h"
 int main() {
-  CompilerContext::TheContext;
+  auto TargetTriple = llvm::sys::getDefaultTargetTriple();
+  llvm::InitializeAllTargetInfos();
+  llvm::InitializeAllTargets();
+  llvm::InitializeAllTargetMCs();
+  llvm::InitializeAllAsmParsers();
+  llvm::InitializeAllAsmPrinters();
+
+  std::string Error;
+  auto Target = llvm::TargetRegistry::lookupTarget(TargetTriple, Error);
+
+  auto CPU = "generic";
+  auto Features = "";
+  llvm::TargetOptions opt;
+  auto TargetMachine = Target->createTargetMachine(TargetTriple, CPU, Features, opt, llvm::Reloc::PIC_);
+
+  CompilerContext::TheModule->setDataLayout(TargetMachine->createDataLayout());
+  CompilerContext::TheModule->setTargetTriple(TargetTriple);
+
+  auto filename = "out.o";
+  std::error_code EC;
+  llvm::raw_fd_ostream dest(filename, EC, llvm::sys::fs::OF_None);
+  llvm::legacy::PassManager pass;
+  // This is the version used by modern LLVM
+  auto FileType = llvm::CGFT_ObjectFile;
+  pass.run(*CompilerContext::TheModule);
+  dest.flush();
+  //CompilerContext::TheContext;
   std::cout<<"Hello LLVM\n";
 }
