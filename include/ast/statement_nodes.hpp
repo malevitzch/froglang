50ae4f636472220f3f49bbe612d04785050c7658
malevitzch <malevitzch@gmail.com>
2025-01-01 14:08:05 +0100

    Implemented some Statement AST stuff

--- include/ast/statement_nodes.hpp
+++ include/ast/statement_nodes.hpp
@@ -3,26 +3,34 @@
 #include "expression_nodes.hpp"
 
 namespace ast {
+
+  // Abstract class
   class StatementNode : public Node {
   private:
   protected:
     StatementNode() = default;
     ~StatementNode() = default;
   public:
+    virtual std::string get_name() override;
   };
 
   // This inherits from Node directly because a bundle of statements is not a statement.
   class Statements : public Node {
   private:
-    std::vector<std::shared_ptr<StatementNode>> statements;
+    std::vector<std::shared_ptr<Node>> statements;
   public:
+    virtual std::string get_name() override;
     virtual llvm::Value* codegen() override;
     void add_statement(std::shared_ptr<StatementNode> statement);
+    virtual std::vector<std::shared_ptr<Node>> get_children() override;
   };
+
   class Block : public StatementNode {
   private:
-    Statements statements;
+    std::shared_ptr<StatementNode> statements;
   public:
+    virtual llvm::Value* codegen() override;
+
   };
 
   class ExpressionStatement : public StatementNode {
--- src/ast/statement_nodes.cpp
+++ src/ast/statement_nodes.cpp
@@ -1,1 +1,20 @@
 #include "ast/statement_nodes.hpp"
+
+namespace ast {
+  
+  std::string StatementNode::get_name() {
+    return "Statement Node";
+  }
+
+  std::string Statements::get_name() {
+    return "Statements Node";
+  }
+  llvm::Value* Statements::codegen() {
+  }
+  void Statements::add_statement(std::shared_ptr<StatementNode> statement) {
+    statements.push_back(statement);
+  }
+  std::vector<std::shared_ptr<Node>> Statements::get_children() {
+    return statements;
+  }
+}
