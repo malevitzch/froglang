e104db0a260f88447bcd8f3c35531f3d355f9143
malevitzch <malevitzch@gmail.com>
2025-01-13 11:06:23 +0100

    Added files for ValueHolder structure

---
+++ include/dsa/named_values.hpp
@@ -0,0 +1,13 @@
+#include <vector>
+#include <unordered_map>
+#include <llvm/IR/Value.h>
+namespace DSA {
+  class ValueHolder {
+  private:
+    std::unordered_map<std::string, std::vector<llvm::Value*>> values;
+  public:
+    llvm::Value* get_val(std::string name);
+    void add_val(std::string name, llvm::Value val);
+    void remove_val(std::string name);
+  };
+}
---
+++ src/dsa/named_values.cpp
@@ -0,0 +1,1 @@
+#include "dsa/named_values.hpp"
