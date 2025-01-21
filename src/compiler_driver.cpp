3d7dcd753a04ac2fd5db293c08391ba5e0fa3547
malevitzch <malevitzch@gmail.com>
2025-01-21 17:24:04 +0100

    Cleaned up the driver code for the compiler, now the default behavior is compiling to executable named 'exec'

--- src/compiler_driver.cpp
+++ src/compiler_driver.cpp
@@ -1,21 +1,14 @@
 #include "compiler.hpp"
+
 #include <fstream>
 
-//FIXME: the driver can be simplified a lot
 int main(int argc, char** argv) {
-  std::ifstream file;
-  if(argc >= 2) {
-    // If a file is provided, open it and set as input stream
-    file = std::ifstream(argv[1]);
-    if(!file.is_open()) {
-      std::cerr << "Cannot open file: " << argv[1] << "\n";
-      return 1;
-    }
-    std::ofstream debug_stream("debug.txt");
-    Compiler compiler(&debug_stream);
-    compiler.compile_to_exec("test", "exec1");
-  } else {
+  if(argc < 2) {
     std::cerr << "No file input given\n";
     return 0;
   }
+  std::string filename = argv[1];
+  std::ofstream debug_stream("debug.txt");
+  Compiler compiler(&debug_stream);
+  compiler.compile_to_exec(filename, "exec");
 }
