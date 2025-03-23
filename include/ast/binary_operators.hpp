9a3e5515ca82f32d704b6c0ea1912fa78c9ccc0f
malevitzch <malevitzch@gmail.com>
2025-03-23 14:09:03 +0100

    Created all the classes needed for the updated binary operators

---
+++ include/ast/binary_operators.hpp
@@ -0,0 +1,62 @@
+#ifndef AST_BINARY_OPERATORS_HPP
+#define AST_BINARY_OPERATORS_HPP
+
+#include "ast/expression_nodes.hpp"
+
+namespace ast {
+
+  class AdditionOperator : BinaryOperator {
+  
+  };
+
+  class SubtractionOperator : BinaryOperator {
+  
+  };
+
+  class MultiplicationOperator : BinaryOperator {
+  
+  }; 
+
+  class DivisionOperator : BinaryOperator {
+  
+  }; 
+
+  class ModuloOperator : BinaryOperator {
+  
+  }; 
+  
+  class LesserComparisonOperator : BinaryOperator {
+  
+  };
+
+  class GreaterComparisonOperator : BinaryOperator {
+  
+  }; 
+
+  class LesserEQComparisonOperator : BinaryOperator {
+  
+  }; 
+
+  class GreaterEQComparisonOperator : BinaryOperator {
+  
+  }; 
+
+  class EqualityComparisonOperator : BinaryOperator {
+  
+  }; 
+
+  class InequalityComparisonOperator : BinaryOperator {
+  
+  }; 
+
+  class LogicalAndOperator : BinaryOperator {
+  
+  }; 
+
+  class LogicalOrOperator : BinaryOperator {
+  
+  };
+
+}
+
+#endif
