abfb6f932f282566a2f564beeb428d52cff26d95
malevitzch <malevitzch@gmail.com>
2025-04-05 10:18:33 +0200

    Removed completed TODO

--- src/ast/node.cpp
+++ src/ast/node.cpp
@@ -4,7 +4,6 @@ namespace ast {
   std::string Node::get_name() {
     return "Uninitialized Name";
   }
-  //TODO: consider this throwing an error or emitting a warning
   std::vector<std::shared_ptr<Node>> Node::get_children() {
     throw std::runtime_error(
       "The method get_children() is not implemented for type "
