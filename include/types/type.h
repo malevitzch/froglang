33c8268f81250601bbdeae09b0c012ae3ee84b35
malevitzch <malevitzch@gmail.com>
2025-04-07 10:42:03 +0200

    Added more declarations to the VariableType class

--- include/types/type.h
+++ include/types/type.h
@@ -1,9 +1,23 @@
+#include <string>
+#include <functional>
+#include <map>
+
+#include "llvm/IR/Value.h"
+
 namespace frtype {
 
   class VariableType {
   private:
-    bool is_mutable = false;
+    const std::string name;
+    const bool mutable_type = false;
+    const bool autocast_type = false;
+    std::map<
+      const std::shared_ptr<VariableType>,
+      const std::function<llvm::Value*(llvm::Value*)>> casts;
   public:
+    std::string get_name() const;
+    bool is_automatically_castable() const;
+    bool is_mutable() const;
   };
 
 }
