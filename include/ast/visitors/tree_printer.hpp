18a073fd8febfc29a3e6cd55ae94c50e0078a60f
malevitzch <malevitzch@gmail.com>
2025-06-14 14:46:27 +0200

    Added functions that make printing stuff with TreePrinter much easier

--- include/ast/visitors/tree_printer.hpp
+++ include/ast/visitors/tree_printer.hpp
@@ -13,6 +13,8 @@ namespace ast {
     std::ostream* output_stream;
     int indent_depth = 0;
     std::string indent();
+    void line(std::initializer_list<std::string> text);
+    void line(std::string text);
   public:
     TreePrinter(std::ostream* output_stream);
     virtual void visit_program_node(ProgramNode& program_node) override;
--- src/ast/visitors/tree_printer.cpp
+++ src/ast/visitors/tree_printer.cpp
@@ -5,8 +5,16 @@ namespace ast {
   std::string TreePrinter::indent() {
     return std::string(indent_depth, '\t');
   }
+  void TreePrinter::line(std::initializer_list<std::string> text) {
+    indent();
+    for(std::string s : text)
+      (*output_stream) << s;
+  }
+  void TreePrinter::line(std::string text) {
+    line({text});
+  }
   TreePrinter::TreePrinter(std::ostream* output_stream) : output_stream(output_stream) {}
   void TreePrinter::visit_program_node(ProgramNode& node) {
-    *output_stream << indent() << "Program Node";
+    line("Program Node");
   }
 }
